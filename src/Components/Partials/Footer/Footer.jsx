8c849893262eb8c5cc01fc8d33f93c2ed29b5997
MariaEhlert <83949695+MariaEhlert@users.noreply.github.com>
2023-07-07 12:32:38 +0200

    opdate

--- src/Components/Partials/Footer/Footer.jsx
+++ src/Components/Partials/Footer/Footer.jsx
@@ -7,8 +7,8 @@ export const Footer = () => {
         <footer className={styles.footerWrapper}>
             <h4>Kontakt mig på</h4>
             <article>
-            <a href="mailto: mariaehlert95@gmail.com" target="_blank"> Email <AiOutlineMail /></a>
-            <a href="https://www.linkedin.com/in/maria-gottlieb-ehlert-ba6474220/" target="_blank">LinkedIn profil <AiFillLinkedin /></a>
+            <a href="mailto: mariaehlert95@gmail.com" target="_blank" rel="noreferrer"> Email <AiOutlineMail /></a>
+            <a href="https://www.linkedin.com/in/maria-gottlieb-ehlert-ba6474220/" target="_blank" rel="noreferrer">LinkedIn profil <AiFillLinkedin /></a>
             </article>
         </footer>
     )
